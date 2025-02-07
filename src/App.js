6a70b0670fef0dd8a0968b816bdaf5d7bf795cf4
ohneRE-L <fazilov.p.i.23@gmail.com>
2025-02-07 12:22:35 +0300

    Задание 7

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (<div>
+    <h1>Задание №1</h1>
+    Привет, JSX!
+    <h1>Задание №2</h1>
+    <p>Первый абзац</p>
+    <p>Второй абзац</p>
+    <p>Третий абзац</p>
+    <h1>Задание №3</h1>
+    <p className="first">Первый абзац</p>
+    <p className="second">Второй абзац</p>
+    <p className="third">Третий абзац</p>
+
+
+  </div>);
+}
+
+export default App;
